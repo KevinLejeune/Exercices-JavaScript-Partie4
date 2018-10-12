db0d544d5b071027e35f8e871f2f39fcd169fba1
Kévin Lejeune <lejeune-kevin@hotmail.fr>
2018-10-12 10:36:20 +0200

    add correction of part one

--- questions.js
+++ questions.js
@@ -1,63 +1,75 @@
 /**
- * Exercice sur les chaines de caractères.
- * Trouvez la façon de faire la plus optimal.
- * Il peut y avoir plusieur façon de faire.
- */
+* Exercice sur les chaines de caractères.
+* Trouvez la façon de faire la plus optimal.
+* Il peut y avoir plusieur façon de faire.
+*/
 var tailleString = function (texte) {
-    return texte.length;
+  var result = texte.length;
+  return result;
 }
 var remplaceECar = function (texte) {
-    return 'A completer';
+  var result = texte.replace('e', ' ');
+  return result;
 }
 var concatString = function (texte1, texte2) {
-    return 'A completer';
+  var result = texte1.concat(texte2);
+  return result;
 }
 var afficherCar5 = function (texte) {
-    return 'A completer';
+  var result = texte.charAt(4);
+  return result;
 }
 var afficher9Car = function (texte) {
-    return 'A completer';
+  var result = texte.slice(0, 9);
+  return result;
 }
 var majusculeString = function (texte) {
-    return 'A completer';
+  var result = texte.toUpperCase();
+  return result;
 }
 var minusculeString = function (texte) {
-    return 'A completer';
+  var result = texte.toLowerCase();
+  return result;
 }
 var SupprEspaceString = function (texte) {
-    return 'A completer';
+  var result = texte.trim();
+  return result;
 }
 var IsString = function (texte) {
-    return 'A completer';
+  var result = typeof(texte) === 'string';
+  return result;
 }
 var AfficherExtensionString = function (texte) {
-    return 'A completer';
+  var result = texte.substring(texte.lastIndexOf("j"));
+  return result;
 }
 var NombreEspaceString = function (texte) {
-    return 'A completer';
+  var result = texte.split(' ').length - 1;
+  return result;
 }
 var InverseString = function (texte) {
-    return 'A completer';
+  var result = texte.split('').reverse().join('');
+  return result;
 }
 
 /**
- * Exercices sur les nombres et les caluls mathématiques
- */
+* Exercices sur les nombres et les caluls mathématiques
+*/
 var calculPuissance = function (x, y) {
-    return 'A completer';
+  return 'A completer';
 }
 var valeurAbsolue = function (nombre) {
-    return 'A completer';
+  return 'A completer';
 }
 var valeurAbsolueArray = function (array) {
-    return 'A completer';
+  return 'A completer';
 }
 var sufaceCercle = function (rayon) {
-    return 'A completer';
+  return 'A completer';
 }
 var hypothenuse = function (ab, ac) {
-    return 'A completer';
+  return 'A completer';
 }
 var calculIMC = function (poids, taille) {
-    return 'A completer';
+  return 'A completer';
 }
